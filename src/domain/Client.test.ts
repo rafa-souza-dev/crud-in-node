63eaf861c322523df97d6eb62f4d72f1acdb5dce
Rafael Souza <rafael.jose@ferreri.co>
2025-05-27 12:31:10 -0300

    add client tests

---
+++ src/domain/Client.test.ts
@@ -0,0 +1,41 @@
+import { describe, expect, it } from "vitest";
+import { ClientProps } from "./Client.types.js";
+import { Client } from "./Client.js";
+
+describe('Client class', () => {
+    describe('with valid props', () => {
+        const props: ClientProps = {
+            id: 1,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+            email: 'juvenal@email.com',
+            name: 'Juvenal',
+            phone: '123'
+        }
+
+        describe('when create a client instance', () => {
+            it('should be able to create a client', () => {
+                const newClient = new Client(props);
+
+                expect(newClient.id).toBe(props.id);
+                expect(Boolean(newClient.createdAt)).toBe(true);
+                expect(Boolean(newClient.updatedAt)).toBe(true);
+            })
+        })
+
+        describe('when update a client instance', () => {
+            it('should be able to update a client', async () => {
+                const newClient = new Client(props);
+                const oldUpdatedAt = newClient.updatedAt;
+
+                await new Promise((res, _) => setTimeout(res, 100))
+
+                newClient.id = 3
+
+                expect(newClient.id).toBe(3);
+                expect(Boolean(newClient.createdAt)).toBe(true);
+                expect(oldUpdatedAt < newClient.updatedAt).toBe(true);
+            })
+        })
+    })
+})
