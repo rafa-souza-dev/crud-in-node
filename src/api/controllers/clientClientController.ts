4cf2657271048d07e6f63f9b2be23d30cf680b8a
Rafael Souza <rafael.jose@ferreri.co>
2025-05-27 22:49:26 -0300

    add createClientController route

--- src/api/controllers/clientClientController.ts
+++ src/api/controllers/clientClientController.ts
@@ -1,5 +1,22 @@
 import { Request, Response } from 'express';
+import { z } from "zod/v4";
 
-export const createClientController = (_: Request, res: Response) => {
-    res.status(201).json();
+import { generateCreateClientDefault } from '../../use-cases/create-client/factories.js';
+import { Client } from '../../domain/Client.js';
+
+export const createClientController = async (req: Request, res: Response) => {
+    const clientCreateSchema = z.object({
+        client: z.object({
+            name: z.string().min(1, 'Nome é obrigatório'),
+            email: z.email('Email inválido'),
+            phone: z.string().regex(
+                /^(\+?[1-9]\d{1,14}|\(\d{2}\) \d{5}-\d{4})$/,
+                'Telefone inválido. Use o formato internacional ou (99) 99999-9999'
+            )
+        })
+    });
+    const data = clientCreateSchema.parse(req.body);
+    const { client } = await generateCreateClientDefault().handle(data.client);
+
+    res.status(201).json({ client: Client.serialize(client) });
 };
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { app } from "./api/app.js";
+import { connectMongoDB } from "./infra/db/connectMongoDB.js";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
-});
+connectMongoDB()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server is running at http://localhost:${PORT}`);
+        });
+    })
