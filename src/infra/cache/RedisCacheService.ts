872a443bb88fb5958b3e0306bb5eee7f978c3472
Rafael Souza <rafael.jose@ferreri.co>
2025-05-28 00:07:02 -0300

    implements RedisCacheService class

---
+++ src/infra/cache/RedisCacheService.ts
@@ -0,0 +1,30 @@
+import { Redis } from 'ioredis';
+
+import { CacheService } from './CacheService.js';
+
+export class RedisCacheService implements CacheService {
+    private static instance: RedisCacheService | null = null;
+    private redis: Redis;
+
+    private constructor(redisHost = 'localhost', redisPort = 6379) {
+        this.redis = new Redis({
+            host: redisHost,
+            port: redisPort,
+        });
+    }
+
+    public static getInstance(redisHost = 'localhost', redisPort = 6379): RedisCacheService {
+        if (!RedisCacheService.instance) {
+            RedisCacheService.instance = new RedisCacheService(redisHost, redisPort);
+        }
+        return RedisCacheService.instance;
+    }
+
+    async get(key: string): Promise<string | null> {
+        return await this.redis.get(key);
+    }
+
+    async set(key: string, value: string, ttl: number): Promise<void> {
+        await this.redis.set(key, value, 'EX', ttl);
+    }
+}
